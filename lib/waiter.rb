ab473603976bc672b75ae62f5f2650e5abd1dba5
Philippe Timothe <philippetimothe@gmail.com>
2020-05-16 00:12:24 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,9 +1,17 @@
 class Waiter
   
+  @@all = [] 
+  
   def initialize(name, years_of_experience)
     @name = name
     @years_of_experience = years_of_experience
+    @@all << self 
   end
+  
+  def self.all 
+    @@all 
+  end 
+  
   
   
   
